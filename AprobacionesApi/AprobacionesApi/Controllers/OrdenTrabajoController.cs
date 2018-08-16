a374b07d077da9969f3660ea644ae6b68d7f2666
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-08-16 12:04:18 -0500

    .

---
+++ AprobacionesApi/AprobacionesApi/Controllers/OrdenTrabajoController.cs
@@ -0,0 +1,21 @@
+﻿using AprobacionesApi.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AprobacionesApi.Controllers
+{
+    public class OrdenTrabajoController : ApiController
+    {
+        EntitiesGeneral db = new EntitiesGeneral();
+        public IEnumerable<VW_ORDENES_TRABAJO> GET()
+        {
+            DateTime Desde = DateTime.Now.Date.AddMonths(-10);
+            IEnumerable<VW_ORDENES_TRABAJO> Lista = db.VW_ORDENES_TRABAJO.Where(q => q.CINV_FECING >= Desde && (q.CINV_ST == "P" || q.CINV_ST == "G") && (q.CINV_TDOC == "OT" || q.CINV_TDOC == "OC")).ToList();
+            return Lista;
+        }
+    }
+}
---
+++ AprobacionesApi/AprobacionesApi/Controllers/UsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using AprobacionesApi.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AprobacionesApi.Controllers
+{
+    public class UsuarioController : ApiController
+    {
+        EntitiesGeneral db = new EntitiesGeneral();
+        public USUARIOS Get(string USUARIO, string CLAVE)
+        {
+            USUARIOS RESULTADO = db.USUARIOS.Where(q => q.USUARIO == USUARIO
+                                  && q.CLAVE == CLAVE).FirstOrDefault();
+            return RESULTADO;
+        }
+    }
+}
