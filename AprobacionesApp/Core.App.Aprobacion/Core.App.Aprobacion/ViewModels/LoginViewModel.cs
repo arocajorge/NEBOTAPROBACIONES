39dffa7aa8cb26178fba89193a04aca825d0bf67
AROCA <AROCA@DESKTOP-V0N0DGJ>
2018-10-19 21:11:25 -0500

    .

--- AprobacionesApp/Core.App.Aprobacion/Core.App.Aprobacion/ViewModels/LoginViewModel.cs
+++ AprobacionesApp/Core.App.Aprobacion/Core.App.Aprobacion/ViewModels/LoginViewModel.cs
@@ -100,6 +100,11 @@
 
             if (ValidarUsuarioServicio && string.IsNullOrEmpty(Settings.UrlConexionActual))
             {
+                Settings.UrlConexionExterna = string.IsNullOrEmpty(Settings.UrlConexionExterna) ? "http://190.110.211.82:20000" : Settings.UrlConexionExterna;
+                Settings.UrlConexionInterna = string.IsNullOrEmpty(Settings.UrlConexionInterna) ? "http://192.168.1.7:20000" : Settings.UrlConexionInterna;
+                Settings.RutaCarpeta = string.IsNullOrEmpty(Settings.RutaCarpeta) ? "/Api" : Settings.RutaCarpeta;
+                Settings.UrlConexionActual = Settings.UrlConexionExterna;
+                /*
                 this.IsEnabled = true;
                 this.IsRunning = false;
                 await Application.Current.MainPage.DisplayAlert(
@@ -107,6 +112,7 @@
                     "Dispositivo no configurado",
                     "Aceptar");
                 return;
+                */
             }
 
             if (ValidarUsuarioServicio)
