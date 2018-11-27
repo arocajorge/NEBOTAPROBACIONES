c5ce14fbc32026a6734b1f7f7684eefec7d5bcda
Jorge Aroca <jaroca@fixed-sa.com>
2018-11-27 13:10:06 -0500

    Update CumplimientoLineasViewModel.cs

--- AprobacionesApp/Core.App.Aprobacion/Core.App.Aprobacion/ViewModels/CumplimientoLineasViewModel.cs
+++ AprobacionesApp/Core.App.Aprobacion/Core.App.Aprobacion/ViewModels/CumplimientoLineasViewModel.cs
@@ -146,7 +146,7 @@ namespace Core.App.Aprobacion.ViewModels
                     q.ImagenJefe = q.EstadoJefe == "P" ? "ic_check_box" : "ic_check_box_outline_blank";
                     q.ImagenSupervisor = q.EstadoSupervisor == "P" ? "ic_check_box" : "ic_check_box_outline_blank";
                     q.Color = q.EstadoJefe == "P" && q.EstadoSupervisor == "P" ? "LightGreen" : 
-                    ((q.EstadoJefe == "P" && q.EstadoSupervisor != "P") || (q.EstadoJefe != "P" && q.EstadoSupervisor == "P") ? "LightYellow" : "White");
+                    ((q.EstadoJefe == "P" && q.EstadoSupervisor != "P") || (q.EstadoJefe != "P" && q.EstadoSupervisor == "P") ? "Yellow" : "White");
                 });
                 _lstBitacora = _lstBitacora.OrderBy(q => q.Linea).ToList();
                 
