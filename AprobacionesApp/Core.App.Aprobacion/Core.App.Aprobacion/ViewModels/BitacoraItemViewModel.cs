227c8d40d94a881c65c089b9cd6be405c53941ff
Jorge Aroca <jaroca@fixed-sa.com>
2018-12-02 22:00:17 -0500

    .

--- AprobacionesApp/Core.App.Aprobacion/Core.App.Aprobacion/ViewModels/BitacoraItemViewModel.cs
+++ AprobacionesApp/Core.App.Aprobacion/Core.App.Aprobacion/ViewModels/BitacoraItemViewModel.cs
@@ -1,7 +1,10 @@
-﻿using Core.App.Aprobacion.Models;
+﻿using Core.App.Aprobacion.Helpers;
+using Core.App.Aprobacion.Models;
+using Core.App.Aprobacion.Services;
 using Core.App.Aprobacion.Views;
 using GalaSoft.MvvmLight.Command;
 using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Core.App.Aprobacion.ViewModels
 {
@@ -37,11 +40,49 @@ namespace Core.App.Aprobacion.ViewModels
             get { return new RelayCommand(Aprobar); }
         }
 
-        private void Aprobar()
+        private async void Aprobar()
         {
-            this.Color = "LightGreen";
-            this.EstadoSupervisor = "P";
-            this.ImagenSupervisor = "ic_check_box";
+            ApiService apiService = new ApiService();
+            Response con = await apiService.CheckConnection(Settings.UrlConexionActual);
+            if (!con.IsSuccess)
+            {
+                string UrlDistinto = Settings.UrlConexionActual == Settings.UrlConexionExterna ? Settings.UrlConexionInterna : Settings.UrlConexionExterna;
+                con = await apiService.CheckConnection(UrlDistinto);
+                if (!con.IsSuccess)
+                {
+                    await Application.Current.MainPage.DisplayAlert(
+                        "Alerta",
+                        con.Message,
+                        "Aceptar");
+                    return;
+                }
+                else
+                    Settings.UrlConexionActual = UrlDistinto;
+            }
+
+            this.Usuario = Settings.IdUsuario;
+            this.Estado = "P";
+
+            var response_sinc = await apiService.Post<BitacoraModel>(
+                Settings.UrlConexionActual,
+                Settings.RutaCarpeta,
+                "BitacorasJefeSup",
+                this);
+
+            if (!response_sinc.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Alerta",
+                    response_sinc.Message,
+                    "Aceptar");
+                return;
+            }else
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Alerta",
+                    "Actualización de estado exitosa",
+                    "Aceptar");
+            }            
         }
         #endregion
     }
